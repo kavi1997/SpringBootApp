d4a23d041f526894dd3a4c924753eb5bd2a3eef4
kavi1997 <37212467+kavi1997@users.noreply.github.com>
2020-06-30 19:09:02 +0530

    Update Assignment5Application.java

--- src/main/java/com/example/demo/Assignment5Application.java
+++ src/main/java/com/example/demo/Assignment5Application.java
@@ -3,11 +3,15 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.builder.SpringApplicationBuilder;
 @SpringBootApplication
-public class Assignment5Application {
+public class Assignment5Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Assignment5Application.class, args);
 	}
-
+@Override
+      protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+          return builder.sources(Assignment5Application.class);
+      }
 }
