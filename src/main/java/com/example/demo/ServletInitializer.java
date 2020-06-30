c8eadc32ee1918cfd1a50b2a283602a88d5fbd82
kavi1997 <37212467+kavi1997@users.noreply.github.com>
2020-06-30 19:17:44 +0530

    Update ServletInitializer.java

--- src/main/java/com/example/demo/ServletInitializer.java
+++ src/main/java/com/example/demo/ServletInitializer.java
@@ -11,6 +11,6 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(SpringAppApplication.class);
+        return application.sources(Assignment5Application.class);
     }
 }
